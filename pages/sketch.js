4c1e02827dea719a4217227250c8330c125ee384
Jessica <jessica.park.021@gmail.com>
2023-05-05 09:57:23 +1200

    5May fixed mistake

--- pages/sketch.js
+++ pages/sketch.js
@@ -2,6 +2,7 @@
 
 var canvas
 
+//window will resize to the width and height of the window
 function windowResized() {
   //console.log('resized')
   resizeCanvas(windowWidth, windowHeight)
@@ -14,12 +15,12 @@ function setup() {
   // background(255)
 }
 
+//pressing a key will clear the canvas
 function keyPressed() {
   clear()
 }
 
 // change colour
-
 let red = 0
 let green = 0
 let blue = 0
@@ -48,4 +49,10 @@ function changeToRed() {
   red = 245
   green = 66
   blue = 147
+}
+
+function changeToBlack() {
+  red = 0
+  green = 0
+  blue = 0
 }
