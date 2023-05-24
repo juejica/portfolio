ab7bbb4cf24cd39d8af92d59f01efaf2ca5f4bbb
Jessica <jessica.park.021@gmail.com>
2023-05-24 18:01:03 +1200

    scarletdrone

--- pages/sketch_z1.js
+++ pages/sketch_z1.js
@@ -188,3 +188,16 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 
 hiddenElements.forEach((el) => observer.observe(el))
+
+//parallax effect
+// document.addEventListener("mousemove", parallax);
+// function parallax(e) {
+//   document.querySelectorAll(".object").forEach(function(move{
+//     var moving_value = move.getAttribute("data-value");
+//     var x = e.clientX * moving_value;
+//     var y = e.clientY * moving_value;
+
+//     move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+
+//   }));
+// }
